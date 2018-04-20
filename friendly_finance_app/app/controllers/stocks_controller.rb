6326f00390d4033467a1ca5a2950ff84bafdaea4
ashleynicole472 <asha.4720@gmail.com>
2018-04-20 14:30:34 -0700

    Fixed issue within stock search box for when an incorrect symbol is searched by a user

--- friendly_finance_app/app/controllers/stocks_controller.rb
+++ friendly_finance_app/app/controllers/stocks_controller.rb
@@ -3,12 +3,17 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_from_lookup(params[:stock])
-      render 'users/my_portfolio'
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:danger] = "Nothing found for what was entered"
+        redirect_to my_portfolio_path
+      end
     else
       flash[:danger] = "Nothing entered within the search"
       redirect_to my_portfolio_path
     end
       #to see the object render json: @stock
   end
-  
+
 end
