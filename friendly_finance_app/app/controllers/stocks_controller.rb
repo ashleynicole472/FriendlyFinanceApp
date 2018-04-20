0c9e31e3f2dea30214a714cd3bd4cd509eda3988
ashleynicole472 <asha.4720@gmail.com>
2018-04-20 14:23:43 -0700

    Fixed search issue if user enters nothing within the stock search box

--- friendly_finance_app/app/controllers/stocks_controller.rb
+++ friendly_finance_app/app/controllers/stocks_controller.rb
@@ -1,8 +1,14 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'users/my_portfolio'
-    #to see the object render json: @stock
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      render 'users/my_portfolio'
+    else
+      flash[:danger] = "Nothing entered within the search"
+      redirect_to my_portfolio_path
+    end
+      #to see the object render json: @stock
   end
+  
 end
